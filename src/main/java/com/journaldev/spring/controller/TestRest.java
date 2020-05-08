783d04cf63fd75623b7ee121d04ddd90d7a783cc
Hamza Belmellouki <hamzabelmellouki123@gmail.com>
2020-05-08 02:02:42 +0000

    Initial commit

---
+++ src/main/java/com/journaldev/spring/controller/TestRest.java
@@ -0,0 +1,43 @@
+package com.journaldev.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TestRest {
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployees() {
+        return Arrays.asList(new Employee("7amra", 21), new Employee("Hamza", 21), new Employee("Nacer", 40));
+    }
+
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
